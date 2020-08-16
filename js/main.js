8e328872320fcce7c1963ebb18961614e9c6f7c7
Pavel <pav.sobolew2015@yandex.ru>
2020-08-17 00:55:50 +0300

    Update swiper

--- js/main.js
+++ js/main.js
@@ -23,15 +23,53 @@ $(document).ready(function () {
 
   })
 
-  $(document).ready(function(){
+  $(document).ready(function () {
     $('.materialboxed').materialbox();
   });
 
-  $(document).ready(function(){
+  $(document).ready(function () {
     $('.sidenav').sidenav({
       edge: 'left'
     });
   });
+
+  // $('.carousel.carousel-slider').carousel({
+  //   fullWidth: false,
+  //   noWrap: true, 
+  //   numVisible: 1
+  // });
+
+  // $('.portfolio-arrow-left').click(() => {
+  //   $('.carousel').carousel('prev');
+  // });
+
+  // $('.portfolio-arrow-right').click(() => {
+  //   $('.carousel').carousel('next');
+  // });
+
+  var mySwiper = new Swiper('.swiper-container', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+    
+  })
+
 
   function scrollToElement(element) {
     $("html").animate({
